305bc18f8f6fa7f6220bf22a97150cc527b8fb6e
iamtruptimane <iamtruptimane@gmail.com>
2023-11-29 12:51:50 +0530

    updated main.tf file

--- main.tf
+++ main.tf
@@ -79,3 +79,48 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
     }
   ])
 }
+# CloudWatch Log Groups
+resource "aws_cloudwatch_log_group" "ecs_cw_log_group" {
+  for_each = toset(keys(var.ecs_services))
+  name     = lower("${each.key}-logs")
+}
+
+# ECS Auto Scaling Configuration
+resource "aws_appautoscaling_target" "service_autoscaling" {
+  for_each = var.ecs_services
+  max_capacity       = each.value.auto_scaling.max_capacity
+  min_capacity       = each.value.auto_scaling.min_capacity
+  resource_id        = "service/${aws_ecs_cluster.ecs_cluster.name}/${aws_ecs_service.service[each.key].name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+# Auto Scaling Policies
+resource "aws_appautoscaling_policy" "ecs_policy_memory" {
+  for_each = var.ecs_services
+  name               = "${var.app_name}-memory-autoscaling"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.service_autoscaling[each.key].resource_id
+  scalable_dimension = aws_appautoscaling_target.service_autoscaling[each.key].scalable_dimension
+  service_namespace  = aws_appautoscaling_target.service_autoscaling[each.key].service_namespace
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+    }
+    target_value = each.value.auto_scaling.memory_threshold
+  }
+}
+resource "aws_appautoscaling_policy" "ecs_policy_cpu" {
+  for_each = var.ecs_services
+  name               = "${var.app_name}-cpu-autoscaling"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.service_autoscaling[each.key].resource_id
+  scalable_dimension = aws_appautoscaling_target.service_autoscaling[each.key].scalable_dimension
+  service_namespace  = aws_appautoscaling_target.service_autoscaling[each.key].service_namespace
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+    target_value = each.value.auto_scaling.cpu_threshold
+  }
+}
